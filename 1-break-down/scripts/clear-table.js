ed3c14fa1c9108e693620664a62365a52868b98f
João Viana <joaopadraoviana@gmail.com>
2018-08-09 10:21:31 +0200

    done

--- 1-break-down/scripts/clear-table.js
+++ 1-break-down/scripts/clear-table.js
@@ -1,1 +1,16 @@
+function clear_table() {
+	var s0_display = document.getElementById("s0");
+	s0_display.innerHTML = "";
 
+	var s1_display = document.getElementById("s1");
+	s1_display.innerHTML = "";
+
+	var s2_display = document.getElementById("s2");
+	s2_display.innerHTML = "";
+	
+	var s3_display = document.getElementById("s3");
+	s3_display.innerHTML = "";
+
+	var sf_display = document.getElementById("sf");
+	sf_display.innerHTML = "";
+};
